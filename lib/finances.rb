017eea1502bee12ca40a826dadd354c7415da3c2
alcesalces11331 <rollin.metzger@gmail.com>
2018-03-07 12:57:30 -0600

    added initial wrapping in finances.rb

--- lib/finances.rb
+++ lib/finances.rb
@@ -1,18 +1,35 @@
 class Receipt
 
-	attr_accessor 
+	attr_accessor :month, :day, :amount, :id, :place
 
-	def initialize
+	def initialize(month, day, place, amount, id = nil)
+		@month = month
+		@day = day
+		@place = place
+		@amount = amount
+		@id = id
 	end
 
 	def self.create_table
 		sql = <<-SQL
-			CREATE TABLE IF NOT EXISTS month (
+			CREATE TABLE IF NOT EXISTS Finances (
 				id INTEGER PRIMARY KEY,
-				day INTEGER
+				month TEXT,
+				day INTEGER,
+				place TEXT,
+				amount INTEGER
 				)
 				SQL
 		DB[:conn].execute(sql)
+	end
+
+	def save
+		sql = <<-SQL
+			INSERT INTO	Finances (month, day, place, amount)
+			VALUES (?, ?)
+			SQL
+
+		DB[:conn].execute(sql, self.month, self.day, self.place, self.amount)
 	end
 
 end
